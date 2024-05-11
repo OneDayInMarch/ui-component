569095344ee21f45d66bccc3861c02447338038e
zenghj <zenghj@gitlab.vsdeep.com>
2024-05-11 15:39:26 +0800

    安装sass tailwind css

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,6 +27,9 @@ const count = ref(0);
     in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
 
 <style scoped>
