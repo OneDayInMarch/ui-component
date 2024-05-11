c68de9be1a023e931f460352ee7250794a84bd88
zenghj <zenghj@gitlab.vsdeep.com>
2024-05-11 19:26:14 +0800

    配置vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueJsx()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    // 支持tsx组件，很关键
+    transformMode: {
+      web: [/\.[jt]sx$/]
+    }
+  }
 })
